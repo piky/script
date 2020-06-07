cbe5269423ac79f1b0faa428c57f3db39d4e0b76
Wijai Thongsom <khainui@gmail.com>
2020-06-07 18:42:42 +0700

    Add reference to firewall.sh

--- bookinfo/firewall.sh
+++ bookinfo/firewall.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-## Master node
+## Reference : https://medium.com/platformer-blog/kubernetes-on-centos-7-with-firewalld-e7b53c1316af
+
+### Master node
 firewall-cmd --permanent --add-port=6443/tcp
 firewall-cmd --permanent --add-port=2379-2380/tcp
 firewall-cmd --permanent --add-port=10250/tcp
@@ -8,10 +10,10 @@ firewall-cmd --permanent --add-port=10252/tcp
 firewall-cmd --permanent --add-port=10255/tcp
 firewall-cmd --permanent --add-port=8472/udp
 firewall-cmd --add-masquerade --permanent
-## only if you want NodePorts exposed on control plane IP as well
+### only if you want NodePorts exposed on control plane IP as well
 firewall-cmd --permanent --add-port=30000-32767/tcp
 
-## Worker nodes
+### Worker nodes
 firewall-cmd --permanent --add-port=10250/tcp
 firewall-cmd --permanent --add-port=10255/tcp
 firewall-cmd --permanent --add-port=8472/udp
